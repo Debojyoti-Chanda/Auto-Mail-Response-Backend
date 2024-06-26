b038994077290ed1fcd1c568bec2bf930047ef90
Debojyoti Chanda <chanda3debojyoti@gmail.com>
2024-06-26 11:13:57 +0530

    Add header data to getEmails Controller

--- routes/googleRoutes.js
+++ routes/googleRoutes.js
@@ -6,6 +6,6 @@ const googleController = require('../controllers/googleController');
 
 router.get('/auth/google', googleController.getAuthUrl);
 router.get('/oauth2callback', googleController.handleOAuth2Callback);
-router.get('/emails', googleController.getEmails);
+router.get('/emails', googleController.getEmails);   //http://localhost:8080/emails?pageToken=YOUR_NEXT_PAGE_TOKEN
 
 module.exports = router;
