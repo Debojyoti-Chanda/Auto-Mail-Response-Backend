9fef00fe98b604faaefb43a4bb792971812851a0
Debojyoti Chanda <chanda3debojyoti@gmail.com>
2024-06-26 09:31:00 +0530

    Create folder structure,index.js and install required packages

---
+++ index.js
@@ -0,0 +1,26 @@
+// app.js
+const express = require('express');
+const session = require('express-session');
+const dotenv = require("dotenv");
+const app = express();
+
+
+
+dotenv.config();//This will load the variables from your .env file into process.env
+
+// Configure session middleware
+const SESSION_SECRET = process.env.SESSION_SECRET;
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } 
+  }));
+
+
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log('Server is running on http://localhost:8080');
+});
